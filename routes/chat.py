ca3a444a232735f9f59f039db532b4beaa9134ce
Curt_H <Curt_H@126.com>
2018-08-13 00:26:45 +0800

    重构: 聊天室首页直接以用户身份登陆聊天室

--- routes/chat.py
+++ routes/chat.py
@@ -3,11 +3,19 @@ from flask import (
     render_template,
     Blueprint,
     request,
+    session,
 )
+from routes import current_user
+from utils import log
 
 chat = Blueprint('chatroom', __name__)
 
 
 @chat.route('/chatroom')
 def chatroom():
-    return render_template('chatroom.html')
+    """
+    聊天室首页
+    """
+    u = current_user()
+    log(f'当前用户\n{u}')
+    return render_template('chatroom.html', u=u)
